0accb22b2207468ca596bacb93e26933afbd79b3
getaudio2 <paugermaisar@hotmail.es>
2024-11-13 20:02:53 +0100

    Added enum class ModelName

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 app = FastAPI()
 
 
@@ -12,3 +17,13 @@ async def read_item():
     return {"item_id": "1",
             "item_name": "Apple",
             "item_price": "3.0$"}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
