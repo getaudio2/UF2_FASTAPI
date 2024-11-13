e50eb0a88329767de31a9fe166d229718fe0c358
getaudio2 <paugermaisar@hotmail.es>
2024-11-13 19:49:00 +0100

    Created GET method to receive item dictionary

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -6,3 +6,9 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/items/")
+async def read_item():
+    return {"item_id": "1",
+            "item_name": "Apple",
+            "item_price": "3.0$"}
