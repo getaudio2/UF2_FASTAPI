5ef6fdb6cfcd61628acb97c773ddcef0afd47905
getaudio2 <paugermaisar@hotmail.es>
2024-11-16 20:30:57 +0100

    Added error handling with response/HTTPException

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response, HTTPException
+from pydantic import BaseModel
 
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
+class Item(BaseModel):
+    id: int
+    name: str
+    description: str | None = None
+    price: float
+    quantity: int
+    brand: str
 
 app = FastAPI()
 
+items = {1: "patata", 2: "queso"}
 
 @app.get("/")
 async def root():
@@ -18,12 +23,16 @@ async def read_item():
             "item_name": "Apple",
             "item_price": "3.0$"}
 
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+@app.get("/items/{id}", status_code=200)
+async def get_item_by_id(id: int, response: Response):
+    if id not in items:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        #raise HTTPException(status_code=404, detail="Aquest item no existeix")
+    return "Aquest item no existeix"
+    #return {"item": items[id]}
 
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
 
-    return {"model_name": model_name, "message": "Have some residuals"}
+@app.post("/itemAdd/", status_code=status.HTTP_201_CREATED)
+async def create_item(item: Item):
+    items[item.id] = item
+    return item
