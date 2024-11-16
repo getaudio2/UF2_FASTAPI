efbd1e0a43e8fe9db13a3af9702592bae22c5d7c
getaudio2 <paugermaisar@hotmail.es>
2024-11-16 20:49:40 +0100

    Added GET response screenshots to READE.md

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -11,27 +11,31 @@ class Item(BaseModel):
 
 app = FastAPI()
 
-items = {1: "patata", 2: "queso"}
+items = {}
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
+# Exercici 2: Mètode GET HTTP que retorna un diccionari amb return
 @app.get("/items/")
 async def read_item():
     return {"item_id": "1",
             "item_name": "Apple",
             "item_price": "3.0$"}
 
-@app.get("/items/{id}", status_code=200)
+@app.get("/itemsResponse/{id}", status_code=200)
 async def get_item_by_id(id: int, response: Response):
     if id not in items:
+        # Exercici 6: mostrar un error 404 amb response si no es troba l'item
         response.status_code = status.HTTP_404_NOT_FOUND
-        #raise HTTPException(status_code=404, detail="Aquest item no existeix")
-    return "Aquest item no existeix"
-    #return {"item": items[id]}
+        return "Aquest item no existeix"
+    return {"item": items[id]}
 
+@app.get("/itemsHttp/{id}", status_code=200)
+async def get_item_by_id(id: int):
+    if id not in items:
+        # Exercici 7: mostrar l'error amb HTTPException
+        raise HTTPException(status_code=404, detail="Aquest item no existeix")
+    return {"item": items[id]}
 
+# Exercici 5: Mètode POST que treballa un item tipus BaseModel
 @app.post("/itemAdd/", status_code=status.HTTP_201_CREATED)
 async def create_item(item: Item):
     items[item.id] = item
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # UF2_FASTAPI
+
+## Prova de mètode GET amb SwaggerUI
+![alt text](captura1.png)
+## Prova de mètode GET amb Postman
+![alt text](captura2.png)
