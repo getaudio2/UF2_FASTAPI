4c52bf2dc18088228ae031458fdd4c0f4aa8c294
getaudio2 <paugermaisar@hotmail.es>
2024-11-20 20:35:16 +0100

    Added connection, read_users and docker files

---
+++ ACTIVITAT9/connection.py
@@ -0,0 +1,17 @@
+import psycopg2 as pg
+
+def create_connection():
+    try:
+        # Conexió a la base de dades
+        # Retorna l'objecte de conexió per la resta de funcions SQL
+        conn = pg.connect(
+            database="postgres",
+            user='admin',
+            password='1234',
+            host='localhost',
+            port='5432'
+        )
+
+        return conn
+    except(Exception, pg.Error) as error:
+        print("Error: ", error)
